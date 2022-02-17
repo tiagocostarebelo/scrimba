ce85a2383b0b65e800991237115fd756f8d9d228
unknown <costarebelo.tiago@gmail.com>
2022-02-17 22:44:23 +0000

    Challenge 4 submitted

---
+++ Valentine's Challenge Week - Challenge 4/app.js
@@ -0,0 +1,28 @@
+document.getElementById("btn").addEventListener("click", filterDuplicates)
+
+const emojis = [
+    "💘","💘","🌞","🧡","🍇",
+    "🧁","⚡️","💝","🐱","💘",
+    "🧡","💫","🍓","💜","⚡️",
+    "⚡️","🐶","💘","🍇","🐱",
+    "💘","💫","🍓","💜","💕",
+    "⚡️","💘","🌞","🐱","💘",
+    "💫","🍓","💜","⚡️","🧡",
+    "💕","🍇","💘"
+    ]
+    
+const allEmojisP = document.getElementById("all-emojis-p")
+const uniqueEmojisP = document.getElementById("unique-emojis-p")
+
+for (let emoji of emojis) {
+    allEmojisP.textContent += emoji
+}
+
+function filterDuplicates() {
+    const uniqueEmojis = new Set(emojis);
+    console.log(uniqueEmojis)
+    const arrayUniqueEmojis = [...uniqueEmojis];
+    console.log(arrayUniqueEmojis)
+    
+    uniqueEmojisP.innerHTML = arrayUniqueEmojis
+}
